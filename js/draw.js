fafaf911e8e3082a3d3ff3f369ee8aebd0d83ebb
Prajul Koshta <prajulkoshta63@gmail.com>
2026-07-05 09:40:23 +0530

    Add circle selection, dragging and resize

--- js/draw.js
+++ js/draw.js
@@ -19,7 +19,9 @@ let isDragging = false;
 
 let dragOffsetX = 0;
 let dragOffsetY = 0;
-
+// Resize
+let isResizing = false;
+const HANDLE_SIZE = 8;
 // ======================================
 // MOUSE DOWN
 // ======================================
@@ -56,15 +58,44 @@ canvas.onmousedown = function (e) {
 
         selectedShape = getShapeAt(startX, startY);
 
-        if (selectedShape) {
+if (selectedShape) {
 
-            isDragging = true;
+    // Circle Handle
+    if (
+        selectedShape.type === "circle" &&
+        isCircleHandle(selectedShape, startX, startY)
+    ) {
 
-            dragOffsetX = startX - selectedShape.x;
-            dragOffsetY = startY - selectedShape.y;
+        console.log("Circle Resize");
 
-            drawAllShapes();
-        }
+        isResizing = true;
+
+    }
+
+    // Rectangle Handle
+    else if (
+        selectedShape.type === "rectangle" &&
+        isBottomRightHandle(selectedShape, startX, startY)
+    ) {
+
+        console.log("Rectangle Resize");
+
+        isResizing = true;
+
+    }
+
+    // Drag
+    else {
+
+        isDragging = true;
+
+        dragOffsetX = startX - selectedShape.x;
+        dragOffsetY = startY - selectedShape.y;
+
+    }
+
+    drawAllShapes();
+}
 
         return;
     }
@@ -91,6 +122,34 @@ canvas.onmousedown = function (e) {
 
 canvas.onmousemove = function (e) {
 
+    // -----------------------
+   // Resize Selected Shape
+   // -------------------------
+     if (currentTool === "select" && isResizing && selectedShape) {
+
+    // Rectangle Resize
+    if (selectedShape.type === "rectangle") {
+
+        selectedShape.width = e.offsetX - selectedShape.x;
+        selectedShape.height = e.offsetY - selectedShape.y;
+
+    }
+
+    // Circle Resize
+    else if (selectedShape.type === "circle") {
+
+        const dx = e.offsetX - selectedShape.x;
+        const dy = e.offsetY - selectedShape.y;
+
+        selectedShape.radius = Math.sqrt(dx * dx + dy * dy);
+
+    }
+
+    drawAllShapes();
+
+    return;
+
+}
     // -----------------------
     // Move Selected Shape
     // -----------------------
@@ -180,12 +239,12 @@ canvas.onmouseup = function (e) {
     if (currentTool === "select") {
 
         isDragging = false;
+        isResizing = false;
 
         saveState();
 
         return;
     }
-
     drawing = false;
 
     if (currentTool === "rectangle") {
@@ -206,6 +265,29 @@ canvas.onmouseup = function (e) {
         });
 
     }
+    if (currentTool === "circle") {
+
+         const radius = Math.sqrt(
+         Math.pow(e.offsetX - startX, 2) +
+         Math.pow(e.offsetY - startY, 2)
+        );
+
+        shapes.push({
+
+         type: "circle",
+
+         x: startX,
+         y: startY,
+ 
+         radius: radius,
+ 
+         color: colorPicker.value,
+         lineWidth: brushSize.value
+
+        });
+
+    }
+    drawAllShapes();
 
     saveState();
 
@@ -222,3 +304,225 @@ canvas.onmouseleave = function () {
     isDragging = false;
 
 };
+// ======================================
+// DRAW ALL SHAPES
+// ======================================
+
+function drawAllShapes() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let shape of shapes) {
+
+        ctx.strokeStyle = shape.color;
+        ctx.lineWidth = shape.lineWidth;
+
+        if (shape.type === "rectangle") {
+
+            ctx.strokeRect(
+                shape.x,
+                shape.y,
+                shape.width,
+                shape.height
+            );
+
+            // Selected Border
+            if (shape === selectedShape) {
+
+               ctx.save();
+
+               ctx.strokeStyle = "blue";
+               ctx.lineWidth = 2;
+
+               ctx.strokeRect(
+                  shape.x - 3,
+                  shape.y - 3,
+                  shape.width + 6,
+                  shape.height + 6
+             );
+
+                 ctx.restore();
+
+                // Draw Handles
+                drawResizeHandles(shape);
+
+            }
+
+        }
+        if (shape.type === "circle")
+             {
+
+             ctx.beginPath();
+
+             ctx.arc(
+             shape.x,
+             shape.y,
+             shape.radius,
+             0,
+            Math.PI * 2
+             );
+
+             ctx.stroke();
+            }
+         // Selected Border
+         if (shape === selectedShape) {
+  
+             ctx.save();
+
+             ctx.strokeStyle = "blue";
+             ctx.lineWidth = 2;
+
+             ctx.beginPath();
+
+             ctx.arc(
+                 shape.x,
+                 shape.y,
+                 shape.radius + 4,
+                 0,
+                 Math.PI * 2
+                );
+
+             ctx.stroke();
+
+             ctx.restore();
+
+       
+
+            }
+           // Circle Resize Handle
+           ctx.fillStyle = "blue";
+
+           ctx.fillRect(
+               shape.x + shape.radius - 4,
+               shape.y - 4,
+               HANDLE_SIZE,
+               HANDLE_SIZE
+            );
+
+
+    }
+}
+
+
+
+
+// ======================================
+// GET SHAPE AT POSITION
+// ======================================
+
+function getShapeAt(x, y) {
+
+    for (let i = shapes.length - 1; i >= 0; i--) {
+
+        const shape = shapes[i];
+
+        if (shape.type === "rectangle") {
+
+            const left = Math.min(shape.x, shape.x + shape.width);
+            const right = Math.max(shape.x, shape.x + shape.width);
+            const top = Math.min(shape.y, shape.y + shape.height);
+            const bottom = Math.max(shape.y, shape.y + shape.height);
+
+            if (
+
+                x >= left &&
+                x <= right &&
+                y >= top &&
+                y <= bottom
+
+            ) {
+
+                return shape;
+
+            }
+
+        }
+
+           if (shape.type === "circle") {
+
+            const dx = x - shape.x;
+            const dy = y - shape.y;
+
+            const distance = Math.sqrt(dx * dx + dy * dy);
+ 
+             if (distance <= shape.radius) {
+
+                 return shape;
+
+                }
+
+            }
+    }
+
+    return null;
+
+}
+// ======================================
+// DRAW RESIZE HANDLES
+// ======================================
+
+function drawResizeHandles(shape) {
+
+    HANDLE_SIZE
+
+    ctx.save();
+
+    ctx.fillStyle = "blue";
+
+    // Top Left
+    ctx.fillRect(shape.x - 4, shape.y - 4, size, size);
+
+    // Top Right
+    ctx.fillRect(shape.x + shape.width - 4, shape.y - 4, size, size);
+
+    // Bottom Left
+    ctx.fillRect(shape.x - 4, shape.y + shape.height - 4, size, size);
+
+    // Bottom Right
+    ctx.fillRect(
+        shape.x + shape.width - 4,
+        shape.y + shape.height - 4,
+        size,
+        size
+    );
+
+    ctx.restore();
+
+}
+function isBottomRightHandle(shape, x, y) {
+
+    return (
+
+        x >= shape.x + shape.width - 6 &&
+        x <= shape.x + shape.width + 6 &&
+        y >= shape.y + shape.height - 6 &&
+        y <= shape.y + shape.height + 6
+
+    );
+
+}
+// ======================================
+// CIRCLE RESIZE HANDLE
+// ======================================
+
+function isCircleHandle(shape, x, y) {
+
+    return (
+
+        x >= shape.x + shape.radius - 6 &&
+        x <= shape.x + shape.radius + 6 &&
+        y >= shape.y - 6 &&
+        y <= shape.y + 6
+
+    );
+
+}
+// ======================================
+// START DRAWING
+// ======================================
+
+function startDrawing() {
+
+    startDrawing();
+
+}
